874ce75bc44d28e79bf77729a31fb752fd829bd7
Lucas Hocihara <lucas.hocihara@gmail.com>
2019-07-09 02:45:37 -0300

    carregando o TCC - alterando estrutura do endpoint /hi

--- m.py
+++ m.py
@@ -5,7 +5,3 @@ app = Flask(__name__)
 @app.route("/hi")
 def boas_vindas():
     return jsonify({"message": "eae meu!"})
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
