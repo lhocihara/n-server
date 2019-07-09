f39b5966dba433ec0b306e743e464dfa1da3a5ce
Lucas Hocihara <lucas.hocihara@gmail.com>
2019-07-09 14:31:59 -0300

    atualização

--- main.py
+++ main.py
@@ -4,14 +4,40 @@ from flask import Flask, jsonify, request, render_template
 from flask_json_schema import JsonSchema, JsonValidationError
 
 app = Flask(__name__)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
 
-schema = JsonSchema(app)
 ##app.config['MONGO_DBNAME'] = 'TCC.Pessoas'
 ##app.config['MONGO_URI'] = 'mongodb+srv://admin_connect:<#_n2noficial_#>@cluster0-hygoa.gcp.mongodb.net/test?retryWrites=true&w=majority'
 mongo = pymongo.MongoClient("mongodb://dev_connect:rgPuzhTgc8HAHFlV@cluster0-hygoa.gcp.mongodb.net/test?retryWrites=true&w=majority")
 db = pymongo.database.Database(mongo, 'TCC')
 dbcol = pymongo.collection.Collection(db, 'TCC.Pessoas')
 
+## ----------------------------------------------------------
+## Rotas padrões
+## ----------------------------------------------------------
+
+## Setando tratamento de erros na validação
+@app.errorhandler(JsonValidationError)
+def validation_error(e):
+    return jsonify({ 'Erro': e.message, 'Errors': [validation_error.message for validation_error in e.errors]})
+
+## Endpoint de boas vindas
+@app.route("/hi")
+def boas_vindas():
+    return render_template("bem_vindos.html")
+
+## Endpoint index
+@app.route("/")
+def index():
+    return render_template("main.html")
+
+## ----------------------------------------------------------
+## Rotas dos serviços para o APP
+## ----------------------------------------------------------
+
+schema = JsonSchema(app)
 ## Definição do schema de validação do Json a ser recebido pela requisição HTTP
 schemaCadastroPessoa = {
   "title": "Pessoa",
@@ -39,30 +65,11 @@ schemaCadastroPessoa = {
   }
 }
 
-
-## Setando tratamento de erros na validação
-@app.errorhandler(JsonValidationError)
-def validation_error(e):
-    return jsonify({ 'Erro': e.message, 'Errors': [validation_error.message for validation_error in e.errors]})
-
 ##Definição do endpoint
 @app.route("/panic", methods=['POST'])
-
 ##O schema a ser validado durante a requisição
 @schema.validate(schemaCadastroPessoa)
 def nao_entre_em_panico():
     if request.headers.get('Authorization') == '42':
         return jsonify({"42": "Nao entra em panico, soh estou fazendo o tcc, e com autorização, hehe!"})
     return jsonify({"message": "Nao entra em panico, soh estou fazendo o tcc, utilizando o tcc!"})
-
-@app.route("/hi")
-def boas_vindas():
-    return render_template("main.html")
-
-@app.route("/")
-def index():
-    return render_template("main.html")
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
